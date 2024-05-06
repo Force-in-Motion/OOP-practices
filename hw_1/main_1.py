966d70e89bec7eaae1187c91117b9fd4df5d219e
Force-in-Motion <canzdao@gmail.com>
2024-05-06 21:07:14 +0300

    create method info animal class Animal

--- hw_1/main_1.py
+++ hw_1/main_1.py
@@ -14,7 +14,23 @@ class Animal:
         self.sound = sound
 
     def print_sound_animal(self):
+        """
+        Выводит в консоль строку с принятым иенем животного его типом и звуком который оно издает
+        :return:
+        """
         print(f'{self.type} с именем {self.name} издает звук: {self.sound}')
         print()
 
-    
+    def print_info_animal(self):
+        """
+        Выводит в консоль всю информацию о животном в созданном объекте на базе класса Animal
+        :return:
+        """
+        print(f'Информационный блок о созданном вами животном :'
+              f'\nВид животного : {self.type}'
+              f'\nИмя животного : {self.name}'
+              f'\nВозраст животного: {self.age}'
+              f'\nЗвук который издает животное: {self.sound}')
+        print()
+
+
