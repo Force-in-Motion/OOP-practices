99c01822b7423b058781ccb883576ace763d9912
Force-in-Motion <canzdao@gmail.com>
2024-05-06 20:44:58 +0300

    create description class Animal

--- hw_1/main_1.py
+++ hw_1/main_1.py
@@ -1,5 +1,14 @@
 
 class Animal:
     def __init__(self, name: str, type: str, age: int, sound: str):
-       
-
+        """
+        Формирует шаблон объекта Animal
+        :param name: Пренимает имя животного
+        :param type: Пренимает вид животного
+        :param age: Пренимает возраст животного
+        :param sound: Пренимает звук, создаваемый животным
+        """
+        self.name = name
+        self.type = type
+        self.age = age
+        self.sound = sound
