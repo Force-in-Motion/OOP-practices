791ca0e2d3003562b2826ff33127b8691e9248c5
Force-in-Motion <canzdao@gmail.com>
2024-05-09 16:50:56 +0300

    create method init class Patient and description

--- hw_2/main_2.py
+++ hw_2/main_2.py
@@ -1,2 +1,14 @@
-class Ananas:
-    pass
+class Patient:
+
+    def __init__(self, full_name: str, age: int, disease: str):
+        """
+        Формирует шаблон объекта Patient
+        :param full_name: Пренимает ФИО пациента
+        :param age: Пренимает возраст пациента
+        :param disease: Пренимает заболевание пациента
+        """
+        self.full_name = full_name
+        self.age = age
+        self.disease = disease
+
+    
