26a39fd7ff382f3bafb77fcc437e629185109f43
Force-in-Motion <canzdao@gmail.com>
2024-05-09 17:12:32 +0300

    create method print_patient_info class Patient and description

--- hw_2/main_2.py
+++ hw_2/main_2.py
@@ -20,5 +20,15 @@ class Patient:
         print(f'Вы успешно записаны на прием на следующую дату: {date}\n')
 
 
+    def print_patient_info(self):
+        print('Данные пациента:\n'
+              f'ФИО пациента {self.full_name}\n'
+              f'Возраст пациента {self.age}\n'
+              f'Текущее заболевание пациента {self.disease}\n')
+
+valera = Patient('Петров Иван Иванович', 42, 'Понос')
+
+valera.makes_an_appointment('24. 12. 1807')
+valera.print_patient_info()
 
 
