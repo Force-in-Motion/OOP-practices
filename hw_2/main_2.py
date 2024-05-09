447617ac23205eba70895adb88377a4556ccb610
Force-in-Motion <canzdao@gmail.com>
2024-05-09 17:44:34 +0300

    create method visiting_place class TouristSpot and description

--- hw_2/main_2.py
+++ hw_2/main_2.py
@@ -1,36 +1,36 @@
-class Patient:
-
-    def __init__(self, full_name: str, age: int, disease: str):
-        """
-        Формирует шаблон объекта Patient
-        :param full_name: Пренимает ФИО пациента
-        :param age: Пренимает возраст пациента
-        :param disease: Пренимает заболевание пациента
-        """
-        self.full_name = full_name
-        self.age = age
-        self.disease = disease
-
-
-    def makes_an_appointment(self, date: str):
-        """
-        Выводит уведомление о записи на прием к врачу на указанную дату
-        :param date: Пренимает дату записи на прием
-        :return:
-        """
-        print(f'Вы успешно записаны на прием на следующую дату: {date}\n')
-
-
-    def print_patient_info(self):
-        print('Данные пациента:\n'
-              f'ФИО пациента {self.full_name}\n'
-              f'Возраст пациента {self.age}\n'
-              f'Текущее заболевание пациента {self.disease}\n')
-
-valera = Patient('Петров Иван Иванович', 42, 'Понос')
-
-valera.makes_an_appointment('24. 12. 1807')
-valera.print_patient_info()
+# class Patient:
+#
+#     def __init__(self, full_name: str, age: int, disease: str):
+#         """
+#         Формирует шаблон объекта Patient
+#         :param full_name: Пренимает ФИО пациента
+#         :param age: Пренимает возраст пациента
+#         :param disease: Пренимает заболевание пациента
+#         """
+#         self.full_name = full_name
+#         self.age = age
+#         self.disease = disease
+#
+#
+#     def makes_an_appointment(self, date: str):
+#         """
+#         Выводит уведомление о записи на прием к врачу на указанную дату
+#         :param date: Пренимает дату записи на прием
+#         :return:
+#         """
+#         print(f'Вы успешно записаны на прием на следующую дату: {date}\n')
+#
+#
+#     def print_patient_info(self):
+#         print('Данные пациента:\n'
+#               f'ФИО пациента: {self.full_name}\n'
+#               f'Возраст пациента: {self.age}\n'
+#               f'Текущее заболевание пациента: {self.disease}\n')
+#
+# valera = Patient('Петров Иван Иванович', 42, 'Понос')
+#
+# valera.makes_an_appointment('24. 12. 1807')
+# valera.print_patient_info()
 
 
 class TouristSpot:
@@ -45,3 +45,14 @@ class TouristSpot:
         self.name_place = name_place
         self.country = country
         self.type_of_attraction = type_of_attraction
+
+
+    def visiting_the_place(self, name: str):
+        """
+        Выводит сообщение о посещении посетителем места
+        :param name:
+        :return:
+        """
+        print(f'Турист {name} посетил {self.name_place}, расположенню в {self.country}\n')
+
+
