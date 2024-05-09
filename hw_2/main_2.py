afca6c1cfc2067b9e1169ebc9eedaa180a352669
Force-in-Motion <canzdao@gmail.com>
2024-05-09 17:57:12 +0300

    create method init class ModelWindow and description

--- hw_2/main_2.py
+++ hw_2/main_2.py
@@ -33,39 +33,61 @@
 # valera.print_patient_info()
 
 
-class TouristSpot:
+# class TouristSpot:
+#
+#     def __init__(self, name_place: str, country: str, type_of_attraction: str):
+#         """
+#          Формирует шаблон объекта TouristSpot
+#         :param name_place: Пренимает название места
+#         :param country: Пренимает страну расположения
+#         :param type_of_attraction: Пренимает тип достопримечательности
+#         """
+#         self.name_place = name_place
+#         self.country = country
+#         self.type_of_attraction = type_of_attraction
+#
+#
+#     def visiting_the_place(self, name: str):
+#         """
+#         Выводит сообщение о посещении посетителем места
+#         :param name:
+#         :return:
+#         """
+#         print(f'Турист {name} посетил {self.name_place}, расположенню в {self.country}\n')
+#
+#
+#     def print_info_name_place(self):
+#         """
+#         Выводит информацию о туристическом месте
+#         :return:
+#         """
+#         print(f'Название места: {self.name_place}\n'
+#               f'Страна расположения: {self.country}\n'
+#               f'Тип достопримечательности: {self.type_of_attraction}')
+#
+#
+# venecia = TouristSpot('Венеция', 'Италия', 'Венециансктй театр')
+# venecia.visiting_the_place('Аркадий')
+# venecia.print_info_name_place()
 
-    def __init__(self, name_place: str, country: str, type_of_attraction: str):
+
+class ModelWindow:
+
+    def __init__(self, name_window: str, coordinates_left_angle: float, horizontal_size: float, verticals_size: float, color_window: str, state_visibility: str, state_frame: str):
         """
-         Формирует шаблон объекта TouristSpot
-        :param name_place: Пренимает название места
-        :param country: Пренимает страну расположения
-        :param type_of_attraction: Пренимает тип достопримечательности
+        Формирует шаблон объекта ModelWindow
+        :param name_window: Пренимает заголовок окна
+        :param coordinates_left_angle: Пренимает координаты левого угла
+        :param horizontal_size: Пренимает размер по горизонтали
+        :param verticals_size: Пренимает размер по вертикали
+        :param color_window: Пренимает цвет окна
+        :param state_visibility: Пренимает состояние: “видимое/невидимое”
+        :param state_frame: Пренимает состояние: “с рамкой/без рамки”
         """
-        self.name_place = name_place
-        self.country = country
-        self.type_of_attraction = type_of_attraction
-
-
-    def visiting_the_place(self, name: str):
-        """
-        Выводит сообщение о посещении посетителем места
-        :param name:
-        :return:
-        """
-        print(f'Турист {name} посетил {self.name_place}, расположенню в {self.country}\n')
-
-
-    def print_info_name_place(self):
-        """
-        Выводит информацию о туристическом месте
-        :return:
-        """
-        print(f'Название места: {self.name_place}\n'
-              f'Страна расположения: {self.country}\n'
-              f'Тип достопримечательности: {self.type_of_attraction}')
-
-
-venecia = TouristSpot('Венеция', 'Италия', 'Венециансктй театр')
-venecia.visiting_the_place('Аркадий')
-venecia.print_info_name_place()
+        self.name_window = name_window
+        self.coordinates_left_angle = coordinates_left_angle
+        self.horizontal_size = horizontal_size
+        self.verticals_size = verticals_size
+        self.color_window = color_window
+        self.state_visibility = state_visibility
+        self.state_frame = state_frame
